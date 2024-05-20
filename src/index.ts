ea0e1c2ec5e3dec3004342d289182722c1e2bc90
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-05-20 12:22:31 +0300

    chore: bump

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,18 @@ export type Stringable =
 
 /** Class-helper for work with formattable [entities](https://core.telegram.org/bots/api#messageentity) */
 export class FormattableString {
+	/** Text of FormattableString (auto covert to it if entities is unsupported)*/
 	text: string;
+	/** Entities of FormattableString */
 	entities: TelegramMessageEntity[];
 
+	/** Create new FormattableString */
 	constructor(text: string, entities: TelegramMessageEntity[]) {
 		this.text = text;
 		this.entities = entities;
 	}
 
+	/** Create new FormattableString */
 	static from(text: string, entities: TelegramMessageEntity[]) {
 		return new FormattableString(text, entities);
 	}
